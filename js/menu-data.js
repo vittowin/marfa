9b43b70e4cc8bc0f502574cfd9eb3b8f11fda7ce
vittowin <smeeshnoi@gmail.com_EMAIL__GITHUB>
2026-08-01 17:33:03 +0300

    Добавить фотографии в каталог

--- js/menu-data.js
+++ js/menu-data.js
@@ -13,29 +13,29 @@ const createCatalogSection = (items, defaults) => items.map((item) => {
 });
 
 const wheatPies = createCatalogSection([
-  ["wheat-cabbage-egg", "Пирожок с капустой и яйцом", 85, 70],
-  ["wheat-apple", "Пирожок с яблоком", 80, 70],
-  ["wheat-cottage-cheese", "Пирожок с творогом", 80, 70],
-  ["wheat-condensed-milk", "Пирожок со сгущёнкой", 80, 70],
-  ["wheat-strawberry", "Пирожок с клубникой", 70, 70],
-  ["wheat-cherry", "Пирожок с вишней", 70, 70],
-  ["wheat-blueberry", "Пирожок с черникой", 70, 70],
+  ["wheat-cabbage-egg", "Пирожок с капустой и яйцом", 85, 70, null, "images/products/pies/pie-cabbage-egg.jpg"],
+  ["wheat-apple", "Пирожок с яблоком", 80, 70, null, "images/products/pies/pie-apple.jpg"],
+  ["wheat-cottage-cheese", "Пирожок с творогом", 80, 70, null, "images/products/pies/pie-cottage-cheese.jpg"],
+  ["wheat-condensed-milk", "Пирожок со сгущёнкой", 80, 70, null, "images/products/pies/pie-condensed-milk.jpg"],
+  ["wheat-strawberry", "Пирожок с клубникой", 70, 70, null, "images/products/pies/pie-strawberry.jpg"],
+  ["wheat-cherry", "Пирожок с вишней", 70, 70, null, "images/products/pies/pie-cherry.jpg"],
+  ["wheat-blueberry", "Пирожок с черникой", 70, 70, null, "images/products/pies/pie-blueberry.jpg"],
   ["wheat-raspberry", "Пирожок с малиной", 70, 70],
-  ["wheat-lingonberry", "Пирожок с брусникой", 70, 70],
-  ["wheat-apricot", "Пирожок с абрикосом", 70, 70],
-  ["wheat-poppy-bun", "Булочка с маком", 80, 70],
-  ["wheat-cinnamon-bun", "Булочка с корицей", 80, 65],
-  ["wheat-rice-egg", "Пирожок с рисом и яйцом", 90, 70],
-  ["wheat-potato", "Пирожок с картошкой", 90, 70],
-  ["wheat-sausage", "Сосиска в тесте", 90, 80],
-  ["wheat-beef-onion", "Пирожок с говядиной и жареным луком", 80, 80],
-  ["wheat-ham-cheese", "Пирожок с ветчиной и сыром", 80, 80],
-  ["wheat-lemon", "Пирожок с лимоном", 80, 70],
-  ["wheat-green-onion-egg", "Пирожок с зелёным луком и яйцом", 75, 70],
-  ["wheat-chicken-onion", "Пирожок с курицей и жареным луком", 80, 80],
-  ["wheat-liver-onion", "Пирожок с говяжьей печенью и жареным луком", 80, 80],
-  ["wheat-carrot-egg", "Пирожок с морковью и яйцом", 90, 70],
-  ["wheat-beef-cabbage", "Пирожок с мясом и капустой", 90, 70]
+  ["wheat-lingonberry", "Пирожок с брусникой", 70, 70, null, "images/products/pies/pie-lingonberry.jpg"],
+  ["wheat-apricot", "Пирожок с абрикосом", 70, 70, null, "images/products/pies/pie-apricot.jpg"],
+  ["wheat-poppy-bun", "Булочка с маком", 80, 70, null, "images/products/pies/pie-poppy.jpg"],
+  ["wheat-cinnamon-bun", "Булочка с корицей", 80, 65, null, "images/products/pies/pie-cinnamon.jpg"],
+  ["wheat-rice-egg", "Пирожок с рисом и яйцом", 90, 70, null, "images/products/pies/pie-rice-egg.jpg"],
+  ["wheat-potato", "Пирожок с картошкой", 90, 70, null, "images/products/pies/pie-potato.jpg"],
+  ["wheat-sausage", "Сосиска в тесте", 90, 80, null, "images/products/pies/pie-sausage.jpg"],
+  ["wheat-beef-onion", "Пирожок с говядиной и жареным луком", 80, 80, null, "images/products/pies/pie-beef-onion.jpg"],
+  ["wheat-ham-cheese", "Пирожок с ветчиной и сыром", 80, 80, null, "images/products/pies/pie-ham-cheese.jpg"],
+  ["wheat-lemon", "Пирожок с лимоном", 80, 70, null, "images/products/pies/pie-lemon.jpg"],
+  ["wheat-green-onion-egg", "Пирожок с зелёным луком и яйцом", 75, 70, null, "images/products/pies/pie-green-onion-egg.jpg"],
+  ["wheat-chicken-onion", "Пирожок с курицей и жареным луком", 80, 80, null, "images/products/pies/pie-chicken-onion.jpg"],
+  ["wheat-liver-onion", "Пирожок с говяжьей печенью и жареным луком", 80, 80, null, "images/products/pies/pie-liver-onion.jpg"],
+  ["wheat-carrot-egg", "Пирожок с морковью и яйцом", 90, 70, null, "images/products/pies/pie-carrot-egg.jpg"],
+  ["wheat-beef-cabbage", "Пирожок с мясом и капустой", 90, 70, null, "images/products/pies/pie-beef-cabbage.jpg"]
 ], {
   category: "pies",
   categoryLabel: "Пшеничная мука",
@@ -44,15 +44,15 @@ const wheatPies = createCatalogSection([
 
 const multigrainPies = createCatalogSection([
   ["multigrain-cabbage", "Пирожок с капустой", 85, 70],
-  ["multigrain-apple", "Пирожок с яблоком", 80, 70],
-  ["multigrain-blueberry", "Пирожок с черникой", 70, 70],
+  ["multigrain-apple", "Пирожок с яблоком", 80, 70, null, "images/products/pies/pie-apple.jpg"],
+  ["multigrain-blueberry", "Пирожок с черникой", 70, 70, null, "images/products/pies/pie-blueberry.jpg"],
   ["multigrain-green-onion-rice", "Пирожок с зелёным луком и рисом", 70, 70],
-  ["multigrain-potato", "Пирожок с картошкой", 85, 70],
+  ["multigrain-potato", "Пирожок с картошкой", 85, 70, null, "images/products/pies/pie-potato.jpg"],
   ["multigrain-buckwheat-onion", "Пирожок с гречей и жареным луком", 85, 70],
-  ["multigrain-beef-onion", "Пирожок с говядиной и жареным луком", 80, 80],
-  ["multigrain-ham-cheese", "Пирожок с ветчиной и сыром", 80, 80],
+  ["multigrain-beef-onion", "Пирожок с говядиной и жареным луком", 80, 80, null, "images/products/pies/pie-beef-onion.jpg"],
+  ["multigrain-ham-cheese", "Пирожок с ветчиной и сыром", 80, 80, null, "images/products/pies/pie-ham-cheese.jpg"],
   ["multigrain-apple-cherry", "Пирожок с яблоком и вишней", 80, 70],
-  ["multigrain-cottage-cheese", "Пирожок с творогом", 80, 70],
+  ["multigrain-cottage-cheese", "Пирожок с творогом", 80, 70, null, "images/products/pies/pie-cottage-cheese.jpg"],
   ["multigrain-vegetable", "Пирожок с овощами", 80, 70],
   ["multigrain-carrot-apple", "Пирожок с морковью и яблоком", 85, 70],
   ["multigrain-rice-lentils", "Пирожок с рисом и чечевицей", 85, 70]
@@ -63,8 +63,8 @@ const multigrainPies = createCatalogSection([
 });
 
 const breads = createCatalogSection([
-  ["bread-wheat-rye-bran", "Хлеб пшенично-ржаной с отрубями", 280, 75],
-  ["bread-gray", "Хлеб серый", 250, 75],
+  ["bread-wheat-rye-bran", "Хлеб пшенично-ржаной с отрубями", 280, 75, null, "images/products/bread/bread-wheat-rye-bran.jpg"],
+  ["bread-gray", "Хлеб серый", 250, 75, null, "images/products/bread/bread-gray.jpg"],
   ["bread-white-loaf", "Хлеб белый, буханка", 230, 75],
   ["bread-white-round", "Хлеб белый круглый", 400, 75],
   ["bread-white-round-small", "Хлеб белый круглый маленький", 180, 45],
@@ -103,15 +103,15 @@ const savoryPies = createCatalogSection([
 });
 
 const sweetPies = createCatalogSection([
-  ["sweet-pie-apple", "Пирог с яблоком", 1300, 950],
-  ["sweet-pie-apple-lingonberry", "Пирог с яблоком и брусникой", 1300, 950],
-  ["sweet-pie-apple-lemon", "Пирог с яблоком и лимоном", 1300, 970],
-  ["sweet-pie-lemon", "Пирог с лимоном", 1100, 990],
-  ["sweet-pie-apricot", "Пирог с абрикосом", 1100, 990],
-  ["sweet-pie-raspberry", "Пирог с малиной", 1100, 990],
-  ["sweet-pie-curd-jam-large", "Пирог творожный с вареньем", 1300, 950, "Начинка на выбор: черника, малина, вишня, брусника, абрикос, клубника, киви, персик, чёрная смородина, мак или изюм."],
-  ["sweet-pie-curd-jam-small", "Пирог творожный с вареньем", 600, 650, "Начинка на выбор: черника, малина, вишня, брусника, абрикос, клубника, киви, персик, чёрная смородина, мак или изюм."],
-  ["sweet-pie-curd-banana-pineapple", "Пирог творожный с бананом или ананасом", 800, 700, "Песочное тесто; начинка с бананом или ананасом на выбор."]
+  ["sweet-pie-apple", "Пирог с яблоком", 1300, 950, null, "images/products/sweet-pies/sweet-pie-apple.jpg"],
+  ["sweet-pie-apple-lingonberry", "Пирог с яблоком и брусникой", 1300, 950, null, "images/products/sweet-pies/sweet-pie-apple-lingonberry.jpg"],
+  ["sweet-pie-apple-lemon", "Пирог с яблоком и лимоном", 1300, 970, null, "images/products/sweet-pies/sweet-pie-apple-lemon.jpg"],
+  ["sweet-pie-lemon", "Пирог с лимоном", 1100, 990, null, "images/products/sweet-pies/sweet-pie-lemon.jpg"],
+  ["sweet-pie-apricot", "Пирог с абрикосом", 1100, 990, null, "images/products/sweet-pies/sweet-pie-apricot.jpg"],
+  ["sweet-pie-raspberry", "Пирог с малиной", 1100, 990, null, "images/products/sweet-pies/sweet-pie-raspberry.jpg"],
+  ["sweet-pie-curd-jam-large", "Пирог творожный с вареньем", 1300, 950, "Начинка на выбор: черника, малина, вишня, брусника, абрикос, клубника, киви, персик, чёрная смородина, мак или изюм.", "images/products/sweet-pies/sweet-pie-curd-jam.jpg"],
+  ["sweet-pie-curd-jam-small", "Пирог творожный с вареньем", 600, 650, "Начинка на выбор: черника, малина, вишня, брусника, абрикос, клубника, киви, персик, чёрная смородина, мак или изюм.", "images/products/sweet-pies/sweet-pie-curd-jam.jpg"],
+  ["sweet-pie-curd-banana-pineapple", "Пирог творожный с бананом или ананасом", 800, 700, "Песочное тесто; начинка с бананом или ананасом на выбор.", "images/products/sweet-pies/sweet-pie-curd-banana-pineapple.jpg"]
 ], {
   category: "sweet-pies",
   categoryLabel: "Сладкий пирог",
