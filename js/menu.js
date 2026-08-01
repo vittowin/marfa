2d229a4f61e37394ad752d456d9599e33d2a3515
vittowin <smeeshnoi@gmail.com_EMAIL__GITHUB>
2026-08-01 13:28:39 +0300

    Добавить фотографии тортов

--- js/menu.js
+++ js/menu.js
@@ -16,6 +16,11 @@ document.addEventListener("DOMContentLoaded", () => {
 
     grid.innerHTML = products.map((product) => `
       <article class="product-card" data-id="${product.id}">
+        ${product.image ? `
+          <div class="product-image">
+            <img src="${product.image}" alt="${product.name}" width="900" height="900" loading="lazy">
+          </div>
+        ` : ""}
         <div class="product-body">
           <div class="product-topline">
             <span class="product-tag product-tag--inline">${product.categoryLabel}</span>
